95733424336d9f472c739b91e50b63ebc1540ca0
Leo Gonzalez <lgonz053@gmail.com>
2019-02-07 08:27:08 -0600

    show is working and displaying

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :client do
     get '/products' => 'products#index'
-    get '/show/:id' => 'products#show'
+    get '/products/:id' => 'products#show'
   end
 end
